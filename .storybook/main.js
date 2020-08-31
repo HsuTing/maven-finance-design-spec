926b0babac88a4b30535db4e21c2798f90d88f5f
HsuTing <hsuting0106@gmail.com>
2020-08-31 14:13:14 +0800

    feat(root): add configs

---
+++ .storybook/main.js
@@ -0,0 +1,22 @@
+module.exports = {
+  stories: ['../stories/**/*.js'],
+  webpackFinal: config => {
+    config.module.rules.push({
+      test: /\.less$/,
+      use: [
+        require.resolve('style-loader'),
+        require.resolve('css-loader'),
+        {
+          loader: require.resolve('less-loader'),
+          options: {
+            lessOptions: {
+              javascriptEnabled: true,
+            },
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
